ad90b610e09604211d7ab06a9ef19b0ef408e506
Roman Miklashevskii <roman.miklashevskiy@gmail.com>
2025-09-04 13:58:32 +0200

    compile expr

--- crates/typed-eval/src/lib.rs
+++ crates/typed-eval/src/lib.rs
@@ -4,27 +4,29 @@ mod expr_parser;
 pub use expr::*;
 pub use expr_parser::*;
 
-pub fn eval_expr(expr: &Expr) -> Result<f64, String> {
+pub type CompiledExpr = Box<dyn Fn() -> f64>;
+
+pub fn compile_expr(expr: &Expr) -> Result<CompiledExpr, String> {
     Ok(match expr {
-        Expr::Int(val) => *val as f64,
-        Expr::Float(val) => *val,
+        &Expr::Int(val) => Box::new(move || val as f64),
+        &Expr::Float(val) => Box::new(move || val),
         Expr::String(_string) => Err("Strings not supported")?,
         Expr::Var(_var_name) => Err("Variables not supported")?,
         Expr::UnOp(op, rhs) => {
-            let rhs = eval_expr(rhs)?;
+            let rhs = compile_expr(rhs)?;
             match op {
-                UnOp::Neg => -rhs,
+                UnOp::Neg => Box::new(move || -rhs()),
                 UnOp::Plus => rhs,
             }
         }
         Expr::BinOp(op, lhs, rhs) => {
-            let lhs = eval_expr(lhs)?;
-            let rhs = eval_expr(rhs)?;
+            let lhs = compile_expr(lhs)?;
+            let rhs = compile_expr(rhs)?;
             match op {
-                BinOp::Add => lhs + rhs,
-                BinOp::Sub => lhs - rhs,
-                BinOp::Mul => lhs * rhs,
-                BinOp::Div => lhs / rhs,
+                BinOp::Add => Box::new(move || lhs() + rhs()),
+                BinOp::Sub => Box::new(move || lhs() - rhs()),
+                BinOp::Mul => Box::new(move || lhs() * rhs()),
+                BinOp::Div => Box::new(move || lhs() / rhs()),
             }
         }
         Expr::FieldAccess(_object, _field_name) => Err("Field access not supported")?,
@@ -42,7 +44,8 @@ pub fn eval(input: &str) -> Result<f64, String> {
             .join(",");
         Err(format!("Error parsing expression: {}", errors))?
     }
-    eval_expr(expr.output().unwrap())
+    let compiled_expr = compile_expr(expr.output().unwrap())?;
+    Ok(compiled_expr())
 }
 
 #[cfg(test)]
