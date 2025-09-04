65fc9a58e6ac55bf460471beb1b5f83221c7d707
Roman Miklashevskii <roman.miklashevskiy@gmail.com>
2025-09-04 21:14:28 +0200

    move move DynFn to dyn_fn module

---
+++ crates/typed-eval/src/dyn_fn.rs
@@ -0,0 +1,64 @@
+use std::{
+    any::{Any, TypeId},
+    ops::Deref,
+};
+
+// the function with a dynamically known type
+pub struct DynFn {
+    pub arg_type: TypeId,
+    pub ret_type: TypeId,
+    boxed_fun: Box<dyn Any>,
+}
+
+impl DynFn {
+    pub fn new<Arg, Ret>(f: impl Fn(&Arg) -> Ret + Clone + 'static) -> Self
+    where
+        Arg: 'static,
+        Ret: 'static,
+    {
+        Self {
+            boxed_fun: Box::new(BoxedFn(Box::new(f))),
+            arg_type: TypeId::of::<Arg>(),
+            ret_type: TypeId::of::<Ret>(),
+        }
+    }
+
+    pub fn downcast<Arg, Ret>(&self) -> Option<BoxedFn<Arg, Ret>>
+    where
+        Arg: 'static,
+        Ret: 'static,
+    {
+        self.boxed_fun.downcast_ref().cloned()
+    }
+}
+
+// the function with a statically known type
+pub struct BoxedFn<Arg, Ret>(Box<dyn ClonableFn<Arg, Ret>>);
+
+impl<Arg, Ret> Clone for BoxedFn<Arg, Ret> {
+    fn clone(&self) -> Self {
+        self.clone_boxed()
+    }
+}
+
+// implementing Deref allows to use the call syntax on the BoxedFn instances
+impl<Arg, Ret> Deref for BoxedFn<Arg, Ret> {
+    type Target = Box<dyn ClonableFn<Arg, Ret>>;
+    fn deref(&self) -> &Self::Target {
+        &self.0
+    }
+}
+
+pub trait ClonableFn<Arg, Ret>: Fn(&Arg) -> Ret {
+    fn clone_boxed(&self) -> BoxedFn<Arg, Ret>;
+}
+
+// implement ClonableFn for every matching function
+impl<Arg, Ret, F> ClonableFn<Arg, Ret> for F
+where
+    F: Fn(&Arg) -> Ret + Clone + 'static,
+{
+    fn clone_boxed(&self) -> BoxedFn<Arg, Ret> {
+        BoxedFn(Box::new(self.clone()))
+    }
+}
--- crates/typed-eval/src/lib.rs
+++ crates/typed-eval/src/lib.rs
@@ -1,73 +1,12 @@
+mod dyn_fn;
 mod expr;
 mod expr_parser;
 
-use std::{
-    any::{Any, TypeId},
-    ops::Deref,
-};
+use std::any::TypeId;
 
+pub use dyn_fn::*;
 pub use expr::*;
 pub use expr_parser::*;
-
-// the function with a statically known type
-pub struct BoxedFn<Arg, Ret>(Box<dyn ClonableFn<Arg, Ret>>);
-
-impl<Arg, Ret> Clone for BoxedFn<Arg, Ret> {
-    fn clone(&self) -> Self {
-        self.clone_boxed()
-    }
-}
-
-// implementing Deref allows to use the call syntax on the BoxedFn instances
-impl<Arg, Ret> Deref for BoxedFn<Arg, Ret> {
-    type Target = Box<dyn ClonableFn<Arg, Ret>>;
-    fn deref(&self) -> &Self::Target {
-        &self.0
-    }
-}
-
-pub trait ClonableFn<Arg, Ret>: Fn(&Arg) -> Ret {
-    fn clone_boxed(&self) -> BoxedFn<Arg, Ret>;
-}
-
-// implement ClonableFn for every matching function
-impl<Arg, Ret, F> ClonableFn<Arg, Ret> for F
-where
-    F: Fn(&Arg) -> Ret + Clone + 'static,
-{
-    fn clone_boxed(&self) -> BoxedFn<Arg, Ret> {
-        BoxedFn(Box::new(self.clone()))
-    }
-}
-
-// the function with a dynamically known type
-pub struct DynFn {
-    boxed_fun: Box<dyn Any>,
-    arg_type: TypeId,
-    ret_type: TypeId,
-}
-
-impl DynFn {
-    pub fn new<Arg, Ret>(f: impl Fn(&Arg) -> Ret + Clone + 'static) -> Self
-    where
-        Arg: 'static,
-        Ret: 'static,
-    {
-        Self {
-            boxed_fun: Box::new(BoxedFn(Box::new(f))),
-            arg_type: TypeId::of::<Arg>(),
-            ret_type: TypeId::of::<Ret>(),
-        }
-    }
-
-    pub fn downcast<Arg, Ret>(&self) -> Option<BoxedFn<Arg, Ret>>
-    where
-        Arg: 'static,
-        Ret: 'static,
-    {
-        self.boxed_fun.downcast_ref().cloned()
-    }
-}
 
 pub trait ExprContext: 'static {
     fn field_getter(field_name: &str) -> Option<fn(&Self) -> f64>;
